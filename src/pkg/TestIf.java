e76fbdc37fe308addad5ea14190730921a0943b8
Hubert <hubert.wawrzynczyk@o2.pl>
2014-05-20 14:48:08 +0200

    Zmiana z 4 na 3

--- src/pkg/TestIf.java
+++ src/pkg/TestIf.java
@@ -2,7 +2,7 @@ package pkg;
 
 public class TestIf {
 	public static void main(String[] args) {
-		int zmienna = 4;	// zmiana z 16 na 4
+		int zmienna = 3;	// zmiana z 4 na 3
 		if (zmienna % 2 == 0)
 			System.out.println("Liczba parzysta");
 		else
