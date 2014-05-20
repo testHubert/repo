8be3a04ccf0965e0232ab7984a4cc7ee48726320
Hubert <hubert.wawrzynczyk@o2.pl>
2014-05-20 14:49:01 +0200

    Zmiana z 3 na -8

--- src/pkg/TestIf.java
+++ src/pkg/TestIf.java
@@ -2,7 +2,7 @@ package pkg;
 
 public class TestIf {
 	public static void main(String[] args) {
-		int zmienna = 3;	// zmiana z 4 na 3
+		int zmienna = -8;	// zmiana z 3 na -8
 		if (zmienna % 2 == 0)
 			System.out.println("Liczba parzysta");
 		else
