52428722556ee666d0885ee114ec498338c5bd1a
Hubert <hubert.wawrzynczyk@o2.pl>
2014-05-20 14:37:42 +0200

    Pierwszy commit

---
+++ src/pkg/TestIf.java
@@ -0,0 +1,17 @@
+package pkg;
+
+public class TestIf {
+	public static void main(String[] args) {
+		int zmienna = 5;
+		if (zmienna % 2 == 0)
+			System.out.println("Liczba parzysta");
+		else
+			System.out.println("Liczba nieparzysta");
+		if (zmienna > 10)
+			System.out.println("Liczba wiêksza od 10");
+		else if (zmienna > 0 && zmienna <= 10)
+			System.out.println("Liczba dodatnia mniejsza, lub równa 10");
+		else
+			System.out.println("Liczba ujemna");
+	}
+}
